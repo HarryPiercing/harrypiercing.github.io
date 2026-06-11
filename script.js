49c921ce2f48e2034799e95b40c2567841d1f050
Harry Seymour <HarryJSeymour@outlook.com>
2026-06-11 01:28:08 +0100

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector(".navbar");
+
+document.addEventListener("click", (e) => {
+    console.log("clicked:", e.target);
+
+    if (e.target.closest("#menu-icon")) {
+        console.log("Burger clicked");
+
+        e.target.closest("#menu-icon").classList.toggle("fa-xmark");
+        navbar.classList.toggle("active");
+    }
+});
